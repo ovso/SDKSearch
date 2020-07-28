8871b82227be0454f1aa8dc47f2b90b2ad5e6db1
jaeho <ovsoce@gmail.com>
2020-07-29 08:07:45 +0900

    test: Add Coroutine test code

--- app/src/test/java/io/github/ovso/sdksearch/ExampleUnitTest.kt
+++ app/src/test/java/io/github/ovso/sdksearch/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package io.github.ovso.sdksearch
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +16,26 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testSomeUI() {
+        runBlocking {
+            launch(Dispatchers.Main) {  // Will be launched in the mainThreadSurrogate dispatcher
+                println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+            }
+        }
     }
 }
