a11c95c8b55cd2402d42755647afd85874ee5071
jaeho <ovsoce@gmail.com>
2020-08-01 05:52:54 +0900

    chore: Change package

--- app/src/main/java/io/github/ovso/sdksearch/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/sdksearch/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.github.ovso.sdksearch
+package io.github.ovso.sdksearch.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import io.github.ovso.sdksearch.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/io/github/ovso/sdksearch/ui/web/WebFragment.kt
+++ app/src/main/java/io/github/ovso/sdksearch/ui/web/WebFragment.kt
@@ -1,10 +1,11 @@
-package io.github.ovso.sdksearch
+package io.github.ovso.sdksearch.ui.web
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.github.ovso.sdksearch.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
